145d2f79b792f577e53776782641a16eea03e8c8
Graham <gpe@openrs2.org>
2023-09-23 09:53:37 +0100

    Switch to Flakes and mkShell

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default = import ./shell.nix {
+        pkgs = nixpkgs.legacyPackages.${system};
+      };
+    });
+}
--- shell.nix
+++ shell.nix
@@ -1,18 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
-(
-  pkgs.buildFHSUserEnv {
-    name = "openrs2-natives";
-    targetPkgs = pkgs: (
-      with pkgs; [
-        gcc
-        jdk11
-        libglvnd.dev
-        maven
-        xorg.libX11.dev
-        xorg.libXt.dev
-        xorg.xorgproto
-      ]
-    );
-  }
-).env
+pkgs.mkShell {
+  name = "openrs2-natives";
+  packages = with pkgs; [
+    gcc
+    jdk11
+    libglvnd.dev
+    maven
+    xorg.libX11.dev
+    xorg.libXt.dev
+    xorg.xorgproto
+  ];
+}
